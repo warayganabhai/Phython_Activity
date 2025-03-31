0b4e27edcdfbb1e1015cae7fc6731c32da0b6df2
Sumayod <mynameiskanekijay5@gmail.com>
2025-04-01 07:51:43 +0800

    Act.1

---
+++ Phython_Activity/Time.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+time = datetime.now()
+CurrentTime = time.strftime("%H,%M,%S")
+
+name = input("Who are you?",)
+print("Hello", name)
+oras = print("The current time is ", CurrentTime)
